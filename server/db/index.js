0ddbfc87d41c65ae97f319add436003998ee4861
Jimmy <jgarcia3000@gmail.com>
2021-05-29 15:39:22 -0700

    Front end

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,14 @@
 // TODO: Establish connection to mysql database
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'pokedex'
+})
+
+db.connect(err => {
+  err ? console.log('mysql suuuuucks') : console.log('Hell yeah mysql works')
+})
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
 // TODO: Create an express server
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = 3000;
+const db = require('./db/index.js')
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/pokemon', (req, res) => {
+  // name, picture, and type
+  let queryStr = `Select pokemon.id, pokemon.name, images.img, types.type from pokemon INNER JOIN images on pokemon.imageNum = images.id INNER JOIN types on pokemon.typeNum = types.id`;
+  db.query(queryStr, (err, results) => {
+    err ? res.status(404).send(err) : res.status(200).send(results)
+  })
+})
+
+app.listen(port, err => {
+  err ? console.log('Not listening') : console.log('We in this bitch')
+})
